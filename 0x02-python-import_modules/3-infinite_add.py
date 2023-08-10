78e46b5e8915bc7e0379af720cb85f7bd035e3db
dideey <bradleyjd32@gmail.com>
2023-08-10 09:58:29 -0700

    add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import sys
+
+
 def main():
     argv = sys.argv[1:]
     total = 0
     for arg in argv:
         total += int(arg)
     print(total)
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
