47c7acd1530854412577eac754cda3e699df7d91
dideey <bradleyjd32@gmail.com>
2023-08-10 09:43:53 -0700

    error check

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-from add_0 import add
-#declarations
+    from add_0 import add
 a = 1
 b = 2
-#printing
 result = add(a, b)
 print("{} + {} = {}".format(a, b, result))
