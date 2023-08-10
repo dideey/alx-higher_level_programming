62e54c0de73749fb38dd44db7253e7a2e1411332
dideey <bradleyjd32@gmail.com>
2023-08-10 09:47:26 -0700

    error check

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-from calculator_1 import add, sub, mul, div
-#defination
+    from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-#output
+
 result1 = add(a, b)
 result2 = sub(a, b)
 result3 = mul(a, b)
 result4 = div(a, b)
-#printing
+
 print("{} + {} = {}".format(a, b, result1))
 print("{} - {} = {}".format(a, b, result2))
 print("{} * {} = {}".format(a, b, result3))
