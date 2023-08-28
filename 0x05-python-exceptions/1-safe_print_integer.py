2a99d88ff72a36e9f91e291dd8b406585fb54d41
dideey <bradleyjd32@gmail.com>
2023-08-28 04:50:05 -0700

    type erroe

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,8 @@
 def safe_print_integer(value):
   try:
     print("{:d}".format(value))
-    print()
     return True
   except (ValueError, TypeError):
     return False
-
+  finally:
+      print()
