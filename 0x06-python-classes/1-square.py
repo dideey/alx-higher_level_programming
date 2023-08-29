be280be98da913de65a2df1f897d8467786c24d1
dideey <bradleyjd32@gmail.com>
2023-08-29 08:16:21 -0700

    python

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""initilaizing a private atribute to the square class"""
+
+
+class Square:
+    """class"""
+    def __innit__(self, size):
+        """mesthod where i initialize the size field"""
+        self.size = size
+        """the size variable"""
