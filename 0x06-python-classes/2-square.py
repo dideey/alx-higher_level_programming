3afd8475a3734d403a3c0493cb32a2eace4e588d
dideey <bradleyjd32@gmail.com>
2023-08-29 08:43:22 -0700

    python

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""initilaizing a private atribute to
+the square class and must be an integer"""
+
+
+class Square:
+    """class"""
+    def __init__(self, size=0):
+        """mesthod where i initialize the size field.
+        Args:
+            size
+        Raises:
+            Typeerror if not int
+            Valueerror if less than zero
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
