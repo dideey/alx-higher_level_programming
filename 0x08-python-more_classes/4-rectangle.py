06c1d09f02641ea4b346653f74df6bb08828fe69
dideey <bradleyjd32@gmail.com>
2023-09-04 11:25:46 -0700

    python

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -63,4 +63,4 @@ class Rectangle:
         return rectang
 
     def __repr__(self):
-        return eval((f"Rectangle({self.width}, {self.height})"))
+        return "Rectangle({}, {})".format(self.__width, self.__height)
