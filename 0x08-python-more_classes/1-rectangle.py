b929c98a950ebdb609fb2b4378fc51fd9a262d71
dideey <bradleyjd32@gmail.com>
2023-09-04 07:44:19 -0700

    python

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,78 +1,42 @@
 #!/usr/bin/python3
-"""a class Rectangle that defines a rectangle
-
-    Raises:
-        TypeError: the rectangle width must be an integer
-        ValueError: the rectangle width must be greater or == 0
-        TypeError: the rectangle height must be an integer
-        ValueError: the rectangle height must be >= 0
-
-    Returns:
-        int: Rectangle height
+"""
+Defining my class Rectangle
 """
 
 
 class Rectangle:
-    """a class Rectangle that defines a rectangle
-    """
+    """Representing my rectangle."""
     def __init__(self, width=0, height=0):
-        """initialization  of private instances
-
+        """Initializing a new Rectangle.
         Args:
-            width (int, optional): Rectangles width. Defaults to 0.
-            height (int, optional): Rectangles height. Defaults to 0.
+            width (int): new rectangle width.
+            height (int): new rectangle height
         """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """getting the with property of the rectangle class
-
-        Returns:
-            int: Rectangle width value
-        """
+        """Getter of the rectangle width."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """setting the rectangle width
-
-        Args:
-            value (int): value to be passed from the instance of rectangle
-
-        Raises:
-            TypeError: the passed value must be an integer
-            ValueError: the passed value must not be a negative value
-        """
-        if not isinstance(value, int):
+        if type(value)is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        if(value)< 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-    @property
+    @property    
     def height(self):
-        """getting the height property of the rectangle class
-
-        Returns:
-            int: Rectangle height value
-        """
+        """Getter of the rectangle height."""
         return self.__height
 
-    @height.setter
+    @height.setter    
     def height(self, value):
-        """setting the rectangle width
-
-        Args:
-            value (int): value to pe passed from the instance of rectangle
-
-        Raises:
-            TypeError: the passed value must be an integer
-            ValueError: the passed value must not be a negative value
-        """
-        if not isinstance(value, int):
-            raise TypeError("height  must be an integer")
-        if value < 0:
-            raise ValueError("height  must be >= 0")
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if(value) < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
