2b37ed68a7a04ea8199d2596eb80462b89d0e5cb
dideey <bradleyjd32@gmail.com>
2023-09-04 17:08:25 -0700

    python

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,79 +1,44 @@
 #!/usr/bin/python3
 import sys
+"""a program that solves the N queens problem
+        The N queens puzzle is the challenge of placing
+        N non-attacking queens on an N×N chessboard.
+"""
 
 
-def is_safe(board, row, col, n):
-    """
-    Check if it's safe to place a queen at a given position on the board.
-
-    Args:
-        board (list): The chessboard represented as a list of lists.
-        row (int): The row to check.
-        col (int): The column to check.
-        n (int): The size of the board.
-
-    Returns:
-        bool: True if it's safe to place a queen; False otherwise.
-    """
-    for i in range(row):
-        if board[i][col] == 'Q':
-            return False
-
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 'Q':
-            return False
-
-    for i, j in zip(range(row, -1, -1), range(col, n)):
-        if board[i][j] == 'Q':
-            return False
-
-    return True
-
-
-def solve_nqueens(n):
-    """
-    Solve the N queens problem and return all possible solutions.
+def intable(s):
+    """_summary_
 
     Args:
-        n (int): The size of the board.
+        s (_type_): _description_
 
     Returns:
-        list: A list of solutions, each represented as a list of strings.
+        _type_: _description_
     """
-    def backtrack(row):
-        if row == n:
-            solutions.append([''.join(row) for row in board])
-            return
-
-        for col in range(n):
-            if is_safe(board, row, col, n):
-                board[row][col] = 'Q'
-                backtrack(row + 1)
-                board[row][col] = '.'
-
-    board = [['.' for _ in range(n)] for _ in range(n)]
-    solutions = []
-    backtrack(0)
-    return solutions
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
     try:
-        n = int(sys.argv[1])
+        return int(s)
     except ValueError:
         print("N must be a number")
-        sys.exit(1)
+    exit(1)
+
+
+def queen():
+    arg_arr = sys.argv
+
+    if len(arg_arr) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+
+    n = intable(arg_arr[1])
+    if not isinstance(n, int):
+        print("N must be a number")
+        exit(1)
 
     if n < 4:
         print("N must be at least 4")
-        sys.exit(1)
+        exit(1)
+    print(n)
 
-    solutions = solve_nqueens(n)
-    for solution in solutions:
-        for row in solution:
-            print(row)
-        print()
+
+if __name__ == "__main__":
+    queen()
