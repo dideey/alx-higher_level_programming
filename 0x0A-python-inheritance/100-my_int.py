c6c5ff147a068ad22c6058ef390dff65ea6bcbe2
dideey <bradleyjd32@gmail.com>
2023-09-11 10:04:25 -0700

    python

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,9 +3,12 @@
 
 
 class MyInt(int):
+"""The class"""
 
     def __eq__(self, other):
+        """return"""
         return not super().__eq__(other)
 
     def __ne__(self, other):
+        """return"""
         return not super().__ne__(other)
