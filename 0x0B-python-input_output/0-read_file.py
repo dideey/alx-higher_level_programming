e9549819bea65e15d5c0229ee613bbeb337a9c58
dideey <bradleyjd32@gmail.com>
2023-09-12 15:31:05 -0700

    python

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,15 +2,7 @@
 """Read file function"""
 
 def read_file(filename=""):
+    """prints"""
 
-    """ with statement used to ensure the file is properly closed after it has been read"""
-
-    """ encoding parameter is set to utf-8"""
-
-    with open(filename, 'r', encoding='utf-8') as f:
-
-        """ read method to read the file"""
-        file_contents = f.read()
-
-        """print the file contents"""
-        print(file_contents, end="")
+    with open(filename, "r", encoding="utf8") as f:
+        print(f.read())
