205143701148d5c0820a04a63a148b1cd058c66d
dideey <bradleyjd32@gmail.com>
2023-09-12 14:58:15 -0700

    python

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Reads a text file and prints to stdout"""
+
+
+def read_file(filename="", encoding="UTF8"):
+    """reads the file"""
+
+    with open(filename="") as f
+    print(f)
