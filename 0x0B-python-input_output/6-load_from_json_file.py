71085dac51d146daa87ccffd95261cb8cdfe9667
dideey <bradleyjd32@gmail.com>
2023-09-12 17:07:25 -0700

    python

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""import json"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """creates in json format"""
+    with open(filename, "r") as f
+    return json.load(f)
