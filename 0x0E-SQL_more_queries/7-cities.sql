ce6ace23e3fe1b1aec7c5d73056b14fc469750c6
dideey <bradleyjd32@gmail.com>
2023-10-18 21:36:43 +0300

    sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    state_id INT FOREIGN KEY(statesid) NOT NULL,
+    state_id INT FOREIGN KEY(state.TABLE.id),
     name VARCHAR(256) NOT NULL
 );
