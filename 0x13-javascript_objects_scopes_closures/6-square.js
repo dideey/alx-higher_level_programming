e0a93e51bfe80fc68ecc12db5a54a833a64ff7c0
dideey <bradleyjd32@gmail.com>
2023-11-14 20:50:50 +0300

    undefined

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,18 @@
 #!/usr/bin/node
 
 const Squares = require('./5-square.js');
-class Square extends Squares{
-    charPrint(c){
-        if(c === undefined) {
-            c = 'X';
-        }
-        for(let i = 0; i < this.height; i++){
-            let display = '';
-            for(let j = 0; j < this.width; j++){
-                display += c;
-            }
-            console.log(display);
-        }
+class Square extends Squares {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
     }
-
+    for (let i = 0; i < this.height; i++) {
+      let display = '';
+      for (let j = 0; j < this.width; j++) {
+        display += c;
+      }
+      console.log(display);
+    }
+  }
 }
 module.exports = Square;
