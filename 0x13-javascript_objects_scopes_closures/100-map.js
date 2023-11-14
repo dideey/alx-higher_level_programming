0e9e4783cdb291c8515809043ed24b5e937c4d27
dideey <bradleyjd32@gmail.com>
2023-11-15 00:03:45 +0300

    vanish mode

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
-// const newlist = list.map((x) => x * list.indexOf(x));
+// const newlist = list.map((x) => x * list.indexOf(x)); i dont get why this is failing
 const newlist = list.map((value, index) => value * index);
 console.log(list);
 console.log(newlist);
