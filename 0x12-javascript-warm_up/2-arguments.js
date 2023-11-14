f4488e1556b0b1266623a8cbde71a3e08f962160
dideey <bradleyjd32@gmail.com>
2023-11-15 02:49:49 +0300

    up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,10 +3,8 @@ const args = process.argv.slice(2);
 const numArgs = args.length;
 if (numArgs === 0) {
   console.log('No argument');
-} 
-else if (numArgs === 1) {
+} else if (numArgs === 1) {
   console.log('Argument found');
-} 
-else {
+} else {
   console.log('Arguments found');
 }
