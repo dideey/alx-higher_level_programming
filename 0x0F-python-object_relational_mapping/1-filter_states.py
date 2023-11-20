4ee2934dbf68ebbac4fa5e77ed97d18f4aeeca4e
dideey <bradleyjd32@gmail.com>
2023-11-20 14:21:17 +0300

    p

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" list all states starting with N """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                               password=sys.argv[2], db=sys.argv[3], port=3306)
+    cursor = database.cursor()
+    cursor.execute('''SELECT * FROM states WHERE name LIKE N%
+                      ORDER BY state.id''')
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    database.close()
