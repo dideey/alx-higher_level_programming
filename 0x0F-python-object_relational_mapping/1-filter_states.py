6e08036608454c385c469bfc9600ccf8edbdee41
dideey <bradleyjd32@gmail.com>
2023-11-20 14:36:16 +0300

    p

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,8 @@ import MySQLdb
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost"user=sys.argv[1],
+                         password=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = db.cursor()
     cursor.execute("SELECT * FROM `states` ORDER BY `id`")
     [print(row) for row in cursor.fetchall() if row[1][0] == "N"]
