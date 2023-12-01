df30f223e260af5020f312086ef3427463134d83
dideey <bradleyjd32@gmail.com>
2023-12-01 20:48:10 +0300

    fetch

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-#fetches a url
+"""fetches a url"""
 import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    page = response.read()
-    print(page)
-    
+
+if __name__ == "__main__":
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        page = response.read()
+
+        print("Body response:")
+        print("\t-type: {}".format(type(page)))
+        print("\t-content: {}".format(page))
+        print("\t-utf8 content: {}".format(page.decode("utf-8")))
