d0e42eb2a66af102dfd1120c8b73f31e8dafce0c
dideey <bradleyjd32@gmail.com>
2024-02-20 22:50:00 +0300

    json prasing

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const req = require("request");
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+req(url + id + '/', (error, response, body)=> {
+    if (error) console.log(error);
+    else console.log(JSON.parse(body).title)
+});
