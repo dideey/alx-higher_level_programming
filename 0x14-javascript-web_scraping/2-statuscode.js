ce0c7e50d36b267633189d8cfc4bbee709cd8ff9
dideey <bradleyjd32@gmail.com>
2024-02-20 21:45:22 +0300

    status

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const rq = require('request');
-rq.get(process.argv[2], (error, response) => {
-  if (error) {
-    console.error('Error :');
-  } else {
-    const statusCode = response.statusCode;
-    console.log('Code:', statusCode);
-  }
-});
+rq(process.argv[2], (error, response) => {
+        if (error) {
+            console.error('Error:', error);
+        } else {
+            const statusCode = response.statusCode;
+            console.log('Code:', statusCode);
+        }
+    });
