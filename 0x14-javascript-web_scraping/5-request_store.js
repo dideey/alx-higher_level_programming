5a2078f2463d3fcfc6185dbbfef179f7ec562e7b
dideey <bradleyjd32@gmail.com>
2024-02-21 03:27:25 +0300

    up

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,6 +5,6 @@ const rq = require('request');
 rq(process.argv[2], (error, response, body) => {
   if (error) console.log('Error :', error);
   fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
-    if (err) console.log("Error:", err)
+    if (err) console.log('Error:', err);
   });
 });
